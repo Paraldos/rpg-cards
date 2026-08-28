355834277c481eef02c1f81559cb20a64a1f42cb
Schiggy <hillbrecht.s@gmail.com>
2026-08-28 12:47:51 +0200

    ability display

--- abilities/ability_display.gd
+++ abilities/ability_display.gd
@@ -1,12 +1,26 @@
+@tool
 extends PanelContainer
 
-@onready var title: Label = %Title
-@onready var cost: Label = %Cost
-@onready var description: Label = %Description
+@onready var title_label: Label = %Title
+@onready var cost_label: Label = %Cost
+@onready var description_label: Label = %Description
 
-@export var ability : Ability
+@export var ability: Ability:
+	set(value):
+		ability = value
+		_update_ability()
 
-func _update():
-	title.text = ability.title
-	cost.text = ability.get_cost_text()
-	description.text = ability.description
+func _ready() -> void:
+	_update_ability()
+
+func _update_ability() -> void:
+	if not is_node_ready():
+		await ready
+	if ability == null:
+		title_label.text = ""
+		cost_label.text = ""
+		description_label.text = ""
+	else:
+		title_label.text = ability.title
+		cost_label.text = "Kosten: %s" % ability.get_cost_text()
+		description_label.text = ability.description
